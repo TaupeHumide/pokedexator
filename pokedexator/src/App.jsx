910dd78005798ba8f249cc818433ba3e8c5125ca
wilder <nicolasrabrioux.nr@gmail.com>
2023-04-04 10:22:19 +0200

    add bulbasor

---
+++ pokedexator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PokemonCard from './components/pokemonCard'
+
+function App() {
+  return (<div>
+    <PokemonCard />
+  </div>
+  );
+}
+
+export default App
